bfff6a9aa4cdebb7388b9b4eee7ec45a8ab1f780
HamdyAboyousef <hamdysalahmohamed.iti@gmail.com>
2025-09-01 22:23:14 +0300

    end of section2

---
+++ Api/Controllers/ProductController.cs
@@ -0,0 +1,37 @@
+using Api.Data;
+using Api.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+
+        private readonly StoreContext context;
+        public ProductController(StoreContext _context)
+        {
+            context = _context;
+
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+
+            return await context.Products.ToListAsync();
+
+        }
+        
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var product = await context.Products.FindAsync(id);
+            if (product == null) return NotFound();
+            return product;
+        }
+    }
+}
---
+++ Api/Data/StoreContext.cs
@@ -0,0 +1,14 @@
+using Api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+        }
+
+        public required DbSet<Product> Products { get; set; }
+    }
+}
